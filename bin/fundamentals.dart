cd0a7e863ae52c64c51d2fe9354f688164fbf443
Daniel <daniel_mingozzi@hotmail.com>
2024-12-27 13:02:55 -0300

    Initialize fundamentals package with basic structure and functionality

---
+++ bin/fundamentals.dart
@@ -0,0 +1,5 @@
+import 'package:fundamentals/fundamentals.dart' as fundamentals;
+
+void main(List<String> arguments) {
+  print('Hello world: ${fundamentals.calculate()}!');
+}
---
+++ test/fundamentals_test.dart
@@ -0,0 +1,8 @@
+import 'package:fundamentals/fundamentals.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
