017d1f5060d80084e6ec5f340bc8d314bf92fa05
Daniel <daniel_profissional1998@hotmail.com>
2025-02-05 11:46:22 -0300

    Implement deposit, withdraw, and transfer methods in Conta class; update main function to demonstrate functionality

--- bin/fundamentals.dart
+++ bin/fundamentals.dart
@@ -1,11 +1,21 @@
 //bin/fundamentals.dart
 import 'package:fundamentals/fundamentals.dart';
-import 'dart:io';
 
 void main() {
   List<Conta> contas = [
-    Conta(titular: Pessoa(nome: "Rafael", cpf: '12342341231', dataNascimento: '21-04-2001')),
-    Conta(titular: Pessoa(nome: "Rafaela", cpf: '00000000000', dataNascimento: '23-07-1998')),
-    Conta(titular: Pessoa(nome: "Marcelo", cpf: '99999999999', dataNascimento: '19-04-1971'))
+    Conta(
+        titular: Pessoa(
+            nome: "Rafael", cpf: '12342341231', dataNascimento: '21-04-2001')),
+    Conta(
+        titular: Pessoa(
+            nome: "Rafaela", cpf: '00000000000', dataNascimento: '23-07-1998')),
+    Conta(
+        titular: Pessoa(
+            nome: "Marcelo", cpf: '99999999999', dataNascimento: '19-04-1971'))
   ];
+
+  contas[1].depositar(100);
+  print("-------------------");
+  contas[1].transferir(50, contas[0]);
+
 }
--- lib/fundamentals.dart
+++ lib/fundamentals.dart
@@ -1,12 +1,45 @@
 //lib/fundamentals.dart
 class Conta {
   final Pessoa titular;
-  double saldo = 0;
+  double _saldo = 0;
   Conta({required this.titular});
 
-  depositar(double value){}
-  sacar(double value){}
-  transferir(double value, Conta conta){}
+  depositar(double value) {
+    print("depositando $value reais na conta da ${titular.nome}");
+    _saldo += value;
+    print("Deposito realizado com sucesso");
+    print("${titular.nome}, valor atualizado: $_saldo");
+  }
+
+  bool sacar(double value) {
+    if (_saldo < value) {
+      print("Operação não efetuada!");
+      print("Saldo insuficiente!");
+      return false;
+    }
+    print("sacando $value reais na conta da ${titular.nome}");
+    _saldo -= value;
+    print("Saque realizado com sucesso");
+    print("${titular.nome}, valor atualizado: $_saldo");
+    return true;
+  }
+
+  bool transferir(double value, Conta conta) {
+    if (_saldo < value) {
+      print("Operação não efetuada!");
+      print("Saldo insuficiente!");
+      return false;
+    }
+    _saldo -= value;
+    conta.depositar(value);
+    print("transferência realizada com sucesso");
+    print("${titular.nome}, valor atualizado: $_saldo");
+    return true;
+  }
+
+  get saldo {
+    return "_";
+  }
 }
 
 class Pessoa {
